75434a2f31cfcb641e886b16de497b1f759e32d9
Ben Davenport-Ray <ben.davenportray@gmail.com>
2013-09-04 22:34:44 -0400

    Added some zsh goodness.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,7 @@
 -- laserdude's xmonad.hs file.
 
+-- Management hooks.
+----------------------------------------------------------------------- 
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -10,10 +12,24 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import System.Exit
 
+-- Other.
 import qualified Data.Map as M 
 import qualified XMonad.StackSet as W
 
--- Management hooks. 
+
+-- Settings.
+----------------------------------------------------------------------- 
+myTerminal = "urxvt"
+myWorkspaces = ["!term", "@net", "#code", "$graph", "%chat", "^fun", "&vid", "*music", "(other"]
+
+-- Statusbars.
+-----------------------------------------------------------------------
+myLeftBar = "dzen2 -w 800 -ta 'l' -xs 1"
+myRightBar = "conky -c ~/dotfiles/dzconkyrc | dzen2 -xs 1 -x 800 -w 800 -ta r"
+
+
+-- Management hooks.
+----------------------------------------------------------------------- 
 myManageHook = composeAll
     [ className =? "Gimp" --> doFloat
     , className =? "Vncviewer" --> doFloat
@@ -21,26 +37,8 @@ myManageHook = composeAll
     , className =? "net-minecraft-LauncherFrame" --> doFloat
     ]
 
-
-main = do
-    dzenleftbar <- spawnPipe myBar
-    dzenrightbar <- spawnPipe "conky -c ~/dotfiles/dzconkyrc | dzen2 -xs 1 -x 800 -w 800 -ta r"
-    xmonad $ defaultConfig{
-        manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , logHook = myLogHook dzenleftbar
-        , terminal = myTerminal
-        , borderWidth = 1
-        , modMask = mod4Mask
-        , workspaces = myWorkspaces
-        , keys = myKeys
-        , handleEventHook = fullscreenEventHook
-        }
-
-myBar = "dzen2 -w 800 -ta 'l' -xs 1"
-myTerminal = "urxvt"
-myWorkspaces = ["!term", "@net", "#code", "$graph", "%chat", "^fun", "&vid", "*music", "(other"]
-
+-- Log hook settings..
+-----------------------------------------------------------------------
 myLogHook proc = dynamicLogWithPP $ defaultPP
     { ppOutput = hPutStrLn proc
     , ppTitle = dzenColor "green" "" . shorten 50
@@ -48,84 +46,40 @@ myLogHook proc = dynamicLogWithPP $ defaultPP
     , ppUrgent = dzenColor "orange" ""
     }
 
-
-
--- Key bindings. Add, modify or remove key bindings here.
---
+-- Key Bindings.
+-----------------------------------------------------------------------
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $ 
-    -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
- 
-    -- launch dmenu
     , ((modm,               xK_p     ), spawn "dmenu_run")
- 
-    -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
- 
-    -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
- 
-     -- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
- 
-    --  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
- 
-    -- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
- 
-    -- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
- 
-    -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
- 
-    -- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp  )
- 
-    -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
- 
-    -- Swap the focused window and the master window
     , ((modm,               xK_Return), windows W.swapMaster)
- 
-    -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
- 
-    -- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
- 
-    -- Shrink the master area
     , ((modm,               xK_h     ), sendMessage Shrink)
- 
-    -- Expand the master area
     , ((modm,               xK_l     ), sendMessage Expand)
- 
-    -- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
- 
-    -- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
- 
-    -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
- 
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
- 
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- Exit xmonad.
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")  -- Restart xmonad
     
-    -- Custom keybindings.
+    -- Applications.
     , ((modm              , xK_v     ), spawn "gvim")             -- v for Vim.
     , ((modm .|. shiftMask, xK_v     ), spawn "urxvt -e vim")     -- V for Vim in console.
     , ((modm              , xK_b     ), spawn "dwb")          -- b for Browser(Chromium).
     , ((modm .|. shiftMask, xK_b     ), spawn "chromium")         -- B for Browser(Firefox).
     , ((modm              , xK_f     ), spawn "urxvt -e ranger")          -- f for Files. 
     , ((modm .|. shiftMask, xK_f     ), spawn "pcmanfm")           -- F for Files (Thunar)
+    
+    -- Media Bar
     , ((0                 , 0x1008ff11), spawn "amixer set Master 5%-")  -- Voldown
     , ((0                 , 0x1008ff13), spawn "amixer set Master 5%+")  -- Volup
     , ((0                 , 0x1008ff12), spawn "amixer set Master toggle")  -- Volmute
@@ -151,5 +105,20 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
-
+-- Main.
+-----------------------------------------------------------------------
+main = do
+    dzenleftbar <- spawnPipe myLeftBar
+    dzenrightbar <- spawnPipe myRightBar 
+    xmonad $ defaultConfig{
+        manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = myLogHook dzenleftbar
+        , terminal = myTerminal
+        , borderWidth = 1
+        , modMask = mod4Mask
+        , workspaces = myWorkspaces
+        , keys = myKeys
+        , handleEventHook = fullscreenEventHook
+        }
 
---
+++ zsh/aliases.zsh
@@ -0,0 +1,2 @@
+alias t = ~/bin/todoc
+alias ping = "ping -c 3"
