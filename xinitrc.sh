7efc8541409d2f55def6bfea832176b435609cee
Ben Davenport-Ray <ben.davenportray@gmail.com>
2013-09-04 22:36:32 -0400

    More goodies. Too lazy to describe ;D

--- xinitrc.sh
+++ xinitrc.sh
@@ -1,24 +1,23 @@
 # laserdude's /.xinitrc
 #
 
-
-# Use the window manager given or 
-windowmgr=$1
-if [[ $windowmgr == "" ]]; then
-    windowmgr="xmonad"
+if [ -d /etc/X11/xinit/xinitrc.d ]; then
+    for f in /etc/X11/xinit/xinitrc.d/*; do
+        [-x "$f"] && . "$f"
+    done
+    unset f
 fi
 
 setupTiledWM(){
     echo "Starting tiling windows"
     nitrogen --restore &
     xscreensaver &
-
-    # tiling window manager specific stuff here.    
+    wmname LG3D
 
     if [[ windowmgr == dwm ]]; then
         conky -c ~/.dwmconkyrc | while read -r; do xsetroot -name "$REPLY"; done &
     fi
-        
+    ~/bin/notify-listener.py &    
 }
 
 setupStandard() {
@@ -31,13 +30,12 @@ setupStandard() {
         conky &    
     fi
 }
-# Set the cursor to the left pointer.
+
 xsetroot -cursor_name left_ptr &
-# Merges the X server resources.
+~/.screenlayout/dualhead.sh 
 xrdb -merge ~/.Xresources &
-~/.screenlayout/dualhead.sh
-#start the window manager.
-case windowmgr in
+
+case $1 in
 cinnamon)
     setupStandard &
     exec gnome-session-cinnamon
@@ -46,8 +44,17 @@ gnome)
     setupStandard &
     exec gnome-session
     ;;
+xfce)
+    setupStandard &
+    exec xfce4-session
+    ;;
+openbox)
+    setupTiledWM &
+    exec openbox-session
+    ;;
 xmonad|*)
     setupTiledWM &
+    # twmnd &
     exec xmonad
     ;;
 esac
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
-alias t = ~/bin/todoc
-alias ping = "ping -c 3"
+alias t="~/bin/todoc"
+alias ping="ping -c 3"
+alias ls="ls --color -xX"
