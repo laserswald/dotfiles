b3c3492cbc7bce73c58091b37928c0756ca6aa9e
Ben Davenport-Ray <ben.davenportray@gmail.com>
2015-07-09 10:30:47 -0400

    Shiny choices for fonts! :3

--- start_bar.sh
+++ start_bar.sh
@@ -1,3 +1,9 @@
-~/dotfiles/bar/bar_top.sh | lemonbar -f "-*-progsole-*-*-*-*-*-*-*-*-*-*-*-*" -f "-*-stlarch-*-*-*-*-*-*-*-*-*-*-*-*"
-~/dotfiles/bar/bar_bottom.sh | lemonbar -b -f "-*-progsole-*-*-*-*-*-*-*-*-*-*-*-*" -f "-*-stlarch-*-*-*-*-*-*-*-*-*-*-*-*"
+# font="-*-progsole-*-*-*-*-*-*-*-*-*-*-*-*"
+# font="-*-proggyclean-*-*-*-*-*-*-*-*-*-*-*-*"
+ font="-*-proggysquare-*-*-*-*-*-*-*-*-*-*-*-*"
+# font="-*-pixelcarnagemonott-*-*-*-*-*-*-*-*-*-*-*-*"
+
+~/dotfiles/bar/bar_top.sh | lemonbar -f $font -f "-*-stlarch-*-*-*-*-*-*-*-*-*-*-*-*" &
+~/dotfiles/bar/bar_bottom.sh | lemonbar -b -f $font -f "-*-stlarch-*-*-*-*-*-*-*-*-*-*-*-*"
+
 
