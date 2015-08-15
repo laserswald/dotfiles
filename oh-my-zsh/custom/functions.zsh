ddab03bcaeead56c67985c123153b1758bae49c5
Ben Davenport-Ray <ben.davenportray@gmail.com>
2015-08-15 18:24:48 -0400

    Added some nice aliases and functions. Whee?

--- oh-my-zsh/custom/functions.zsh
+++ oh-my-zsh/custom/functions.zsh
@@ -1,1 +1,8 @@
 ding () { echo "\a" }
+function aa {
+    echo "alias $1=$2" >> ~/etc/oh-my-zsh/custom/aliases.zsh
+    source ~/.zshrc
+}
+function search {
+    w3m "https://duckduckgo.com/?q=$1"
+}
