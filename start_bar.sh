527c064ad3ab230f78a0549454083fa2e6246d67
Ben Davenport-Ray <ben.davenportray@gmail.com>
2015-09-07 11:12:07 -0400

    Added start_bar.

--- start_bar.sh
+++ start_bar.sh
@@ -1,5 +1,5 @@
 . ~/etc/theme.sh
-~/etc/bar/bar_top.sh | lemonbar -b -d -f $font -f "-*-stlarch-*-*-*-*-*-*-*-*-*-*-*-*" 
+~/etc/bar/bar_top.sh | lemonbar -d -b -f $font -f "-*-stlarch-*-*-*-*-*-*-*-*-*-*-*-*" 
 # ~/etc/bar/bar_bottom.sh | lemonbar -b -f $font -f "-*-stlarch-*-*-*-*-*-*-*-*-*-*-*-*"
 
 
