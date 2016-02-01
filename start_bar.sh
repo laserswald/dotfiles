0400354c9b94d9c55a2148844f81754fda68f7dc
Ben Davenport-Ray <ben.davenportray@gmail.com>
2016-01-31 22:59:07 -0500

    Made homebase use the minimalist bar.

--- start_bar.sh
+++ start_bar.sh
@@ -1,10 +1,8 @@
 . ~/etc/theme.sh
 
-mkfifo barcmds.ffo
-
 if [[ $HOSTNAME == "homebase" ]]; then
     echo "homebase bar starting..."
-    ~/etc/bar/bar_homebase.sh | lemonbar -b -d -f "$font" -f "$symbolfont" | while read ln; do eval "$ln"; done &
+    ~/etc/bar/minimalist.sh | lemonbar -b -d -g 50x60+3+98 -f "$font" -f "$symbolfont" | while read ln; do eval "$ln"; done &
 else
     ~/etc/bar/bar_any.sh | lemonbar -b -d -f "$font" -f "$symbolfont" | while read ln; do eval "$ln"; done &
 fi
