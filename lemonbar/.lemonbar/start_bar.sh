7de9cde0a8b6620b3f8ddc5077625edd755b0617
Ben Davenport-Ray <ben.davenportray@gmail.com>
2016-02-01 02:15:41 -0500

    Fixed some theming issues

--- lemonbar/.lemonbar/start_bar.sh
+++ lemonbar/.lemonbar/start_bar.sh
@@ -1,9 +1,8 @@
-. ~/etc/theme.sh
+. ~/.laserlights/theme.sh
 
 if [[ $HOSTNAME == "homebase" ]]; then
-    echo "homebase bar starting..."
-    ~/etc/bar/minimalist.sh | lemonbar -b -d -g 50x60+3+98 -f "$font" -f "$symbolfont" | while read ln; do eval "$ln"; done &
+    ~/.lemonbar/minimalist.sh | lemonbar -b -d -g 50x60+3+98 -f "$font" -f "$symbolfont" | while read ln; do eval "$ln"; done &
 else
-    ~/etc/bar/bar_any.sh | lemonbar -b -d -f "$font" -f "$symbolfont" | while read ln; do eval "$ln"; done &
+    ~/.lemonbar/bar_any.sh | lemonbar -b -d -f "$font" -f "$symbolfont" | while read ln; do eval "$ln"; done &
 fi
 
