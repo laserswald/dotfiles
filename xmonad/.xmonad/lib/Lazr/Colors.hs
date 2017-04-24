7b2f2db4e99301cd30aba26ab6ead28cf053f386
Ben Davenport-Ray <ben.davenportray@gmail.com>
2017-04-24 13:37:32 -0400

    Added colors! Yay!

---
+++ xmonad/.xmonad/lib/Lazr/Colors.hs
@@ -0,0 +1,19 @@
+module Lazr.Colors (
+    getColor
+) where
+
+import Control.Monad
+import Data.Char
+import System.Environment
+import System.IO
+import System.Process
+import XMonad.Util.Run
+
+rstrip = reverse . dropWhile isSpace . reverse
+
+getColor :: String -> IO String
+getColor colorname = do
+    homedir <- getEnv "HOME"
+    let tc = homedir ++ "/bin/tcolor"
+    out <- runProcessWithInput tc [colorname] []
+    return $ rstrip out
