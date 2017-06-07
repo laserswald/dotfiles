bd171bc48964cdc436a41e27a851f6626f87891b
Ben Davenport-Ray <ben.davenportray@gmail.com>
2017-06-07 12:32:26 -0400

    Fix to ssh agent

--- sh/.sh.d/modules/sshagent.sh
+++ sh/.sh.d/modules/sshagent.sh
@@ -1,7 +1,31 @@
+#!/bin/sh
+# Start up an SSH agent so I don't have to write the keys all the time
+
 if ! pgrep -u $USER ssh-agent > /dev/null; then
     ssh-agent > ~/.ssh-agent-thing
 fi
 if [[ "$SSH_AGENT_PID" == "" ]]; then
     eval $(<~/.ssh-agent-thing)
 fi
-ssh-add -l >/dev/null || alias ssh='ssh-add -l >/dev/null || ssh-add && unalias ssh; ssh'
+
+EXTRA_KEYS="~/.ssh/ahi_rs"
+
+ssh_add_extras() {
+    echo "Adding extra SSH keys to your keyring..."
+    for f in $EXTRA_KEYS
+    do
+        [ -f "$f" ] && ssh-add "$f"
+    done
+}
+
+ssh_setup_keys() {
+    if ! ssh-add -l >/dev/null; then
+        ssh-add
+        ssh_add_extras
+    fi
+}
+
+if ! ssh-add -l >/dev/null; then
+    alias ssh='ssh_setup_keys && unalias ssh;ssh'
+fi
+
