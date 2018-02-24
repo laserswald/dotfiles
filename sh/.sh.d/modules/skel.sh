3176a2160fb441030091cb66f635149fd0fd5160
Ben Davenport-Ray <ben.davenportray@gmail.com>
2018-02-24 17:05:05 -0500

    Add improvements to skel

--- sh/.sh.d/modules/skel.sh
+++ sh/.sh.d/modules/skel.sh
@@ -8,6 +8,10 @@ skel() {
     template="$1"
     target="$2"
 
+	if ! [ "$@" ]; then
+		echo "usage: $0 TEMPLATE_NAME TARGET_DIR"
+	fi
+
     if [ ! -e "$SKELDIR/$template" ]; then
         echo "skel: no template '$template'" >&2
         return 1
@@ -15,11 +19,21 @@ skel() {
 
     cp -r "$SKELDIR/$template" "$target"
 
+	if test -x "$target/.skel-init"; then
+		"$target/.skel-init"	
+	fi
+
     return 0
 }
 
-cskel() {
-    cp -r "$2" "$SKELDIR/$1"
+mkskel() {
+	if ! [ "$@" ]
+	then
+		echo "usage: $0 TARGET_DIR SKELETON_NAME"
+		echo "Create a skeleton from a sample file or directory."
+	fi
+
+    cp -r "$1" "$SKELDIR/$2"
 }
 
 lsskel() {
