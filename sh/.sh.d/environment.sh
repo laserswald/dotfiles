2360447f3204aaf87e409eb1a0836aebe4ce9ec8
Ben Davenport-Ray <ben.davenportray@gmail.com>
2018-05-31 11:25:09 -0400

    Add local bin directory to PATH

--- sh/.sh.d/environment.sh
+++ sh/.sh.d/environment.sh
@@ -33,4 +33,4 @@ export MAIL="$HOME/var/mail"
 export ORG_PATH="$HOME/org"
 export PUNCH_FILE="$ORG_PATH/.punches"
 
-export PATH="$HOME/bin:$PATH"
+export PATH="$XDG_RUNTIME_DIR/bin:$HOME/bin:$PATH"
