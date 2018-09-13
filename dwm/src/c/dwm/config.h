ff3b10dfb5e64a08abc6f157fe65927757e4a9f9
Ben Davenport-Ray <ben.davenportray@gmail.com>
2018-09-13 08:09:04 -0400

    dwm: Rice is nice.

--- dwm/src/c/dwm/config.h
+++ dwm/src/c/dwm/config.h
@@ -1,25 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "artwiz lemon:pixelsize=10" };
+static const char dmenufont[]       = "artwiz lemon:pixelsize=10";
+
+#include "themes/lander-green.h"
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfg,    normbg,    normborder },
+	[SchemeSel]  = { selfg,     selbg,     selborder  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -31,10 +29,11 @@ static const Rule rules[] = {
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
+
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -56,7 +55,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "/bin/sh", "-c", "dmenu_cmd", NULL };
+static const char *dmenucmd[] = { "/bin/sh", "-c", "dmenu_run", NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
 static Key keys[] = {
---
+++ dwm/src/c/dwm/themes/gentheme.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+template='
+static const char normfg[] = "$WHITE";
+static const char normbg[] = "$BLACK";
+static const char normborder[] = "$BLACK";
+static const char selfg[] = "$BRWHITE";
+static const char selbg[] = "$GREEN";
+static const char selborder[] = "$GREEN";
+'
+
+export BLACK=$(tcolor 'black')
+export RED=$(tcolor 'red')
+export GREEN=$(tcolor 'green')
+export YELLOW=$(tcolor 'yellow')
+export BLUE=$(tcolor 'blue')
+export MAGENTA=$(tcolor 'purple')
+export CYAN=$(tcolor 'cyan')
+export WHITE=$(tcolor 'white')
+export BRBLACK=$(tcolor 'brblack')
+export BRRED=$(tcolor 'brred')
+export BRGREEN=$(tcolor 'brgreen')
+export BRYELLOW=$(tcolor 'bryellow')
+export BRBLUE=$(tcolor 'brblue')
+export BRMAGENTA=$(tcolor 'brpurple')
+export BRCYAN=$(tcolor 'brcyan')
+export BRWHITE=$(tcolor 'brwhite')
+export FOREGROUND=$(tcolor 'foreground')
+export BACKGROUND=$(tcolor 'background')
+export CURSOR="#ffffff"
+export REVCURSOR="#000000"
+
+echo "$template" | envsubst
