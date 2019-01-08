b1e34d6714a18eb3f9b78c333df4fe08e0c123f1
Ben Davenport-Ray <bdavenportray@azaleahealth.com>
2019-01-08 15:32:08 -0500

    Fixed prompt for bash

--- sh/.sh.d/prompt.sh
+++ sh/.sh.d/prompt.sh
@@ -1,3 +1,4 @@
+
 fblack=`tput setaf 0`
 fred=`tput setaf 1`
 fgreen=`tput setaf 2`
@@ -8,40 +9,70 @@ fcyan=`tput setaf 6`
 fwhite=`tput setaf 7`
 normal=`tput sgr0`
 
+# Set up nonprinting character support.
+# 
+# 
+promptfirst=""
+npstart=""
+npend=""
+
+case $SHELL in
+    *bash)
+		npstart="\["
+		npend="\]"
+        ;;
+    *ksh) 
+        promptfirst=$(print \\001)
+		npstart=$promptfirst
+		npend=$promptfirst
+        ;;
+esac
+
+nonprint() {
+    printf "%s%s%s" $npstart $1 $npend
+}
+
+fg() {
+    printf "%s%s%s" "$(nonprint $1)" "$2" "$(nonprint $normal)"
+}
+
+## Different prompt components
+
 prompt_dir () {
-    printf " %s" "$(pwd | sed 's:'$HOME':~:g')"
+    printf "%s" "$(pwd | sed 's:'$HOME':~:g')"
 }
 
 prompt_git () {
     if [ $(git rev-parse --abbrev-ref HEAD 2>/dev/null) ]; then
-		printf " ${fyellow}%s${normal}" $(git rev-parse --abbrev-ref HEAD)
+		printf " %s " $(fg $fyellow "($(git rev-parse --abbrev-ref HEAD))")
 	fi
 }
 
 prompt_status_color () {
 	if [ "$?" -eq 0 ]; then
-		printf "${fgreen}"
+		nonprint $fgreen
 	else
-		printf "${fred}"
+        nonprint $fred
 	fi
 }
+
+## Generate the prompt string
 
 prompt () {
 	statusc=$(prompt_status_color)
-	printf "%s" "${fblue}$(hostname)${normal} $(prompt_dir)$(prompt_git) ${statusc}\$${normal} "
+	printf '%s' "$(fg ${fblue} $(hostname)) $(prompt_dir)$(prompt_git)${statusc}\$$(nonprint $normal) "
 }
 
-prompt_last_status () {
-	char="$1"
-	if test $? -eq 0; then
-		echo " ${fgreen}:${normal}"
-	else
-		echo " ${fred}:${normal}"
-	fi
-}
+case $SHELL in
+    *bash)
+        export PROMPT_COMMAND='export PS1=$(prompt)'
+        ;;
+    *ksh) 
+		# Micro
+		export PS1="\$(prompt)"
+        ;;
+esac
 
-# Micro
-export PS1="\$(prompt)"
 
 [ $TERM = "dumb" ] && export PS1='$ '
 
