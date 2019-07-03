c840a3307e30c8a7b1fc83a03eae347ec35cb18d
Ben Davenport-Ray <ben.davenportray@gmail.com>
2019-07-03 13:21:45 -0400

    Add media play/pause tools and bindings for dwm

--- dwm/src/c/dwm/applications.h
+++ dwm/src/c/dwm/applications.h
@@ -11,6 +11,7 @@ static const char *fmcmd[]         = INST_CMD("ranger");
 static const char *lockcmd[]       = { "slock", NULL };
 static const char *scratchpadcmd[] = { TERMINAL_BIN, "-t", scratchpadname, NULL };
 static const char *searchcmd[]     = { "/usr/bin/env", "surf-ddg", NULL };
+static const char *mailcmd[]       = INST_CMD("neomutt");
 
 /* Media key commands. */
 
@@ -19,3 +20,6 @@ static const char *dispdown[]      = { "xbacklight", "-", "10", NULL };
 static const char *volup[]         = { "amixer", "set", "Master", "10+", NULL };
 static const char *voldown[]       = { "amixer", "set", "Master", "10-", NULL };
 static const char *volmute[]       = { "amixer", "set", "Master", "toggle", NULL };
+static const char *media_play[]    = { "/usr/bin/env", "dj", "toggle", NULL };
+static const char *media_prev[]    = { "/usr/bin/env", "dj", "prev", NULL };
+static const char *media_next[]    = { "/usr/bin/env", "dj", "next", NULL };
