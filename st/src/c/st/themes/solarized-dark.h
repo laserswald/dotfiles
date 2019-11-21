73d62d3686ca72c6a2763a2a008b0ee9f2aaf0b8
Ben Davenport-Ray <ben.davenportray@gmail.com>
2019-11-21 18:10:20 -0500

    St: themes

---
+++ st/src/c/st/themes/solarized-dark.h
@@ -0,0 +1,32 @@
+
+/* Terminal colors (16 first used in escape sequence) */
+const char *colorname[] = {
+	/* 8 normal colors */
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+
+	/* 8 bright colors */
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"",
+	"",
+	"#ffffff",
+	"#000000",
+};
+
