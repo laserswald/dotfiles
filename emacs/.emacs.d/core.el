99ddcf9c2e677fd65ca30a8fa188743be7335fd7
Ben Davenport-Ray <ben.davenport-ray@meraki.net>
2023-02-22 16:03:28 -0500

    Add magit bindings to evil-collection

--- emacs/.emacs.d/core.el
+++ emacs/.emacs.d/core.el
@@ -12,9 +12,9 @@
   (evil-set-initial-state 'dired-mode 'normal)
   ;; Vim-vinegar replacement
   (define-key evil-normal-state-map (kbd "-")
-    '(lambda ()
-       (interactive)
-       (find-file "."))))
+    #'(lambda ()
+        (interactive)
+        (find-file "."))))
 
 (use-package general
   :ensure t
@@ -27,7 +27,7 @@
   :after evil
 
   :init
-  (setq evil-collection-mode-list '(dired))
+  (setq evil-collection-mode-list '(dired magit))
 
   :config
   (evil-collection-init))
