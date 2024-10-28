2d59e2bf1a14e1166fa7dfb35cb21c7acf186542
Ben Davenport-Ray <ben.davenportray@gmail.com>
2024-10-28 11:54:31 -0400

    Delete old vim installation stuff

--- vim/install.bat
+++
@@ -1,9 +0,0 @@
-rem install.bat
-rem
-rem Installs links for my vim files.
-
-rem Remove all the old stuff
-del %HOMEPATH%\vimfiles
-
-rem Install the link so it will stay put automatically
-mklink /d %HOMEPATH%\vimfiles %~dp0.vim
--- vim/windows_install.sh
+++
@@ -1,5 +0,0 @@
-rm ~/_vimrc
-cp ./.vimrc ~/_vimrc 
-rm -rf ~/vimfiles
-cp -r ./.vim ~/vimfiles
-for fi
