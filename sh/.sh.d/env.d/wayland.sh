337926cadb8fb68bccecf145268db95ece3384c4
Ben Davenport-Ray <ben.davenportray@gmail.com>
2025-03-04 13:28:52 -0500

    Wayland: use gl instead of vulkan for gtk4 applications

--- sh/.sh.d/env.d/wayland.sh
+++ sh/.sh.d/env.d/wayland.sh
@@ -3,5 +3,9 @@ if [ -n "$WAYLAND_DISPLAY" ]
 then
 	export MOZ_ENABLE_WAYLAND=1
 	export MOZ_DBUS_REMOTE=1
+
+	# Fixes an issue with GTK 4, Vulkan and AMDGPU.
+	# See https://bbs.archlinux.org/viewtopic.php?id=302001
+	export GSK_RENDERER=gl
 fi
 
