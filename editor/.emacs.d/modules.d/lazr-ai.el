9fa7d1fa52264fe71cc8b7ec122f0e786e66ff08
Ben Davenport-Ray <ben.davenportray@gmail.com>
2025-12-20 12:16:10 -0500

    Emacs: Setting up AI features.
    Add some backends that connect to remote services, especially OpenRouter.
    
    However, by default, I want to use a local model, and Ollama is probably
    the easiest one to set up.

--- editor/.emacs.d/modules.d/lazr-ai.el
+++ editor/.emacs.d/modules.d/lazr-ai.el
@@ -1,26 +1,97 @@
+;;; lazr-ai.el --- Configuration for AI integrations  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025 Ben Davenport-Ray
+
+;; Author: Ben Davenport-Ray <me@lazr.space>
+;; Keywords: ai
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This module installs and configures gptel, an LLM integration package,
+;; for Emacs. We set up several LLM backends to choose from, from commercially
+;; hosted models supplied through OpenRouter, to locally hosted LLMs using
+;; either Llamafile or the Ollama frontend service.
+
+;;; Code:
 
 (require 'lazr-core)
+(require 'lazr-keybindings)
+(require 'password-store) ; for my openrouter password
 
-(use-package gptel
-  :init
+;; Install gptel if we don't have it installed already.
+(lazr/require-package 'gptel)
 
-  ;; OpenRouter offers an OpenAI compatible API
-  (setq gptel-backend
-        (gptel-make-openai "OpenRouter"
-          :host "openrouter.ai"
-          :endpoint "/api/v1/chat/completions"
-          :stream t
-          :models '(openai/gpt-3.5-turbo
-                    mistralai/mixtral-8x7b-instruct
-                    meta-llama/codellama-34b-instruct
-                    codellama/codellama-70b-instruct
-                    google/palm-2-codechat-bison-32k
-                    google/gemini-pro)))
+;;;
+;;; Define some backends to supply model access to us.
+;;;
 
-  (gptel-make-openai "Llamafile"
+(defconst lazr/openrouter-backend
+  (gptel-make-openai "OpenRouter"
+    :host "openrouter.ai"
+    :key (lambda () (password-store-get "services/openrouter.ai/key"))
+    :endpoint "/api/v1/chat/completions"
+    :stream t
+    :models '(mistralai/devstral-2512:free 
+              google/gemini-2.5-flash))
+  "An LLM backend that uses OpenRouter to dispatch between many hosted LLM services")
+
+(defconst lazr/llamafile-backend
+  (gptel-make-openai "Local"
     :stream t
     :protocol "http"
-    :host "localhost:8080"
-    :models '(deepseek-r1)))
+    :host "localhost:8080")
+  "An LLM backend that uses a locally running Llamafile listening on port 8080.")
+
+(defconst lazr/ollama-backend
+  (gptel-make-ollama "Ollama"
+    :stream t
+    :host "localhost:11434"
+    :models '(devstral
+              gemma2))
+  "An LLM backend that uses the Ollama local model service, listening at 11434.")
+
+;;;
+;;; Define some presets, a feature that bundles 
+;;;
+
+;; explain will ask the LLM to explain the current code.
+(gptel-make-preset 'explain
+  :system "Explain what this code does."
+  :backend "Ollama"
+  :model 'devstral)
+  
+;; Set the default backend to ollama.
+(setq gptel-backend lazr/ollama-backend)
+
+;;;
+;;; Set up keybindings for sending data to the LLM for querying.
+;;;
+
+;; Global bindings for bringing up the chat interface, the menu for
+;; modifying and sending a prompt, etc.
+(lazr/leader-map :infix "l"
+  "l" 'gptel
+  "," 'gptel-menu
+  "r" 'gptel-rewrite)
+
+;; Add key mappings for the interactive chat mode.
+(lazr/local-leader-map :keymaps 'gptel-mode-map
+  "," 'gptel-send ; comma comma sends it
+  "a" 'gptel-abort) ; cancel cancel cancel cancel
 
 (provide 'lazr-ai)
+;;; lazr-ai.el ends here
+
